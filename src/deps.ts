822e65727ccc89af0b4175f952dcfd1045f84a62
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-10-04 16:16:37 +0200

    Fjerner ubrukt import

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,6 @@
 import createDagpengerTokenDings, {
   DagpengerTokenDings,
 } from "./dagpengerTokenDings";
-import config from "./config";
 
 export interface Dependencies {
   dagpengerTokenDings: Promise<DagpengerTokenDings>;
