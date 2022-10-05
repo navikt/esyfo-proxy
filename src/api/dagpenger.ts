8901aa13dc2236d8ef10b5e8ab05cb093ea33e45
Jostein Holje <jostein.holje@nav.no>
2022-10-05 12:52:41 +0200

    f Flere ruter i dagpenger api

--- src/api/dagpenger.ts
+++ src/api/dagpenger.ts
@@ -1,27 +1,37 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import config from '../config';
 import { Auth } from '../tokenx/dagpengerTokenDings';
 import { proxyHttpCall } from '../http';
 
-// const VEDTAK_URL = `${DAGPENGER_INNSYN_URL}/vedtak`;
-// const PABEGYNTE_SOKNADER_URL = `${DAGPENGER_INNSYN_URL}/paabegynte`;
-
 function dagpengerRoutes(createDagpengerTokenDings: Auth, dagpengerInnsynUrl = config.DAGPENGER_INNSYN_URL) {
     const SOKNAD_URL = `${dagpengerInnsynUrl}/soknad`;
+    const VEDTAK_URL = `${dagpengerInnsynUrl}/vedtak`;
+    const PABEGYNTE_SOKNADER_URL = `${dagpengerInnsynUrl}/paabegynte`;
+
     const router = Router();
 
-    router.get('/dagpenger/soknad', async (req, res) => {
-        try {
-            const idPortenToken = req.cookies[config.NAV_COOKIE_NAME];
-            const tokenSet = await createDagpengerTokenDings.exchangeIDPortenToken(idPortenToken);
-            const token = tokenSet.access_token;
-            await proxyHttpCall(SOKNAD_URL, {
-                headers: { Authorization: `Bearer ${token}`, TokenXAuthorization: `Bearer ${token}` },
-            })(req, res);
-        } catch (err) {
-            res.status(500).end();
-        }
-    });
+    const getTokenXHeaders = async (req: Request) => {
+        const idPortenToken = req.cookies[config.NAV_COOKIE_NAME];
+        const tokenSet = await createDagpengerTokenDings.exchangeIDPortenToken(idPortenToken);
+        const token = tokenSet.access_token;
+        return { Authorization: `Bearer ${token}`, TokenXAuthorization: `Bearer ${token}` };
+    };
+
+    const dagpengerCall = (url: string) => {
+        return async (req: Request, res: Response) => {
+            try {
+                await proxyHttpCall(url, {
+                    headers: await getTokenXHeaders(req),
+                })(req, res);
+            } catch (err) {
+                res.status(500).end();
+            }
+        };
+    };
+
+    router.get('/dagpenger/soknad', dagpengerCall(SOKNAD_URL));
+    router.get('/dagpenger/vedtak', dagpengerCall(VEDTAK_URL));
+    router.get('/dagpenger/paabegynte', dagpengerCall(PABEGYNTE_SOKNADER_URL));
 
     return router;
 }
