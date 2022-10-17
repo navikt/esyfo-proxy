9e65e9e51d471ab6e7ab22da2283f985ca794a82
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-10-17 13:03:16 +0200

    Endrer navn på database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-export DATABASE_URL=$NAIS_DATABASE_AIA_BACKEND_PROFIL_URL
-node /src/node_modules/prisma/build/index.js migrate dev --schema=/src/prisma/schema.prisma
+export DATABASE_URL=$NAIS_DATABASE_AIA_BACKEND_AIA_BACKEND_URL
 npm run prisma:migrate
 node ./build/index.js
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,6 @@ export interface IEnvironmentVariables {
     DAGPENGER_INNSYN_URL: string;
     PTO_PROXY_URL: string;
     NAIS_CLUSTER_NAME: string;
-    NAIS_DATABASE_AIA_BACKEND_PROFIL_URL: string;
     BASE_PATH: string;
     MELDEKORT_APP_NAME: string;
     MELDEKORT_URL: string;
