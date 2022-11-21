918a5b329411413e6f39f439931e8185f1ef3622
Jonas Enge <jonas.maccyber.enge@nav.no>
2022-11-21 15:31:42 +0100

    f legger til prometheus metrics

--- src/api/health.ts
+++ src/api/health.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
+import { createMetrics } from './metrics';
 
 function healthRoutes() {
     const router = Router();
+    const register = createMetrics();
 
     router.get('/internal/isAlive', (_, res) => {
         res.send('ALIVE');
     });
     router.get('/internal/isReady', (_, res) => {
         res.send('READY');
+    });
+    router.get('/internal/prometheus', async (_, res) => {
+        res.setHeader('Content-type', register.contentType);
+        res.send(await register.metrics());
     });
 
     return router;
