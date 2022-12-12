479657c464a9e8ed0bee34ee16707044bdfe4195
Jostein Holje <jostein.holje@nav.no>
2022-12-12 13:24:55 +0100

    f Legger til indekser på database-tabeller

---
+++ prisma/migrations/20221212122406_indekser/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "AutomatiskReaktivering_bruker_id_idx" ON "AutomatiskReaktivering"("bruker_id");
+
+-- CreateIndex
+CREATE INDEX "BehovForVeiledning_bruker_id_idx" ON "BehovForVeiledning"("bruker_id");
+
+-- CreateIndex
+CREATE INDEX "Profil_bruker_id_idx" ON "Profil"("bruker_id");
