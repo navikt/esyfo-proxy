e37f5da211ade8a9884b70817c8891401858416c
Jostein Holje <jostein.holje@nav.no>
2023-02-23 13:25:19 +0100

    Legg til threshold dato i batch script

--- src/batch/dump-db-to-kafka.ts
+++ src/batch/dump-db-to-kafka.ts
@@ -5,9 +5,25 @@ const prismaClient = new PrismaClient();
 const kafkaProducer = await createProducer();
 
 console.log('Starter med å dumpe automatiskReaktivering...');
-await kafkaProducer.send(await prismaClient.automatiskReaktivering.findMany());
+await kafkaProducer.send(
+    await prismaClient.automatiskReaktivering.findMany({
+        where: {
+            created_at: {
+                lt: new Date(),
+            },
+        },
+    })
+);
 console.log('Ferdig med automatiskReaktivering');
 
 console.log('Starter med å dumpe automatiskReaktiveringSvar...');
-await kafkaProducer.send(await prismaClient.automatiskReaktiveringSvar.findMany());
+await kafkaProducer.send(
+    await prismaClient.automatiskReaktiveringSvar.findMany({
+        where: {
+            created_at: {
+                lt: new Date(),
+            },
+        },
+    })
+);
 console.log('Ferdig med automatiskReaktiveringSvar');
