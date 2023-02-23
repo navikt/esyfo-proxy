7ac4a6243f9107b80852895aa9a6f826c703d195
Jostein Holje <jostein.holje@nav.no>
2023-02-23 13:29:56 +0100

    Chatty batch jobb

--- src/batch/dump-db-to-kafka.ts
+++ src/batch/dump-db-to-kafka.ts
@@ -4,26 +4,29 @@ import createProducer from '../kafka/automatisk-reaktivert-producer';
 const prismaClient = new PrismaClient();
 const kafkaProducer = await createProducer();
 
-console.log('Starter med å dumpe automatiskReaktivering...');
-await kafkaProducer.send(
-    await prismaClient.automatiskReaktivering.findMany({
-        where: {
-            created_at: {
-                lt: new Date(),
-            },
+console.log('Starter med å hente automatiskReaktivering...');
+const automatiskReaktivering = await prismaClient.automatiskReaktivering.findMany({
+    where: {
+        created_at: {
+            lt: new Date(),
         },
-    })
-);
-console.log('Ferdig med automatiskReaktivering');
+    },
+});
+console.log(`Fant ${automatiskReaktivering.length} rader`);
+console.log('Dumper automatiskReaktivering på kafka...');
+await kafkaProducer.send(automatiskReaktivering);
+console.log('Ferdig med automatiskReaktivering\n\n\n');
 
-console.log('Starter med å dumpe automatiskReaktiveringSvar...');
-await kafkaProducer.send(
-    await prismaClient.automatiskReaktiveringSvar.findMany({
-        where: {
-            created_at: {
-                lt: new Date(),
-            },
+console.log('Starter med å hente automatiskReaktiveringSvar...');
+const automatiskReaktiveringSvar = await prismaClient.automatiskReaktiveringSvar.findMany({
+    where: {
+        created_at: {
+            lt: new Date(),
         },
-    })
-);
+    },
+});
+
+console.log(`Fant ${automatiskReaktiveringSvar.length} rader`);
+console.log('Dumper automatiskReaktiveringSvar på kafka...');
+await kafkaProducer.send(automatiskReaktiveringSvar);
 console.log('Ferdig med automatiskReaktiveringSvar');
