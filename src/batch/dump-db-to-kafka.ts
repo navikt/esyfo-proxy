76e4e3031be48f718f913b0f6ef7577b4a540b4a
Jostein Holje <jostein.holje@nav.no>
2023-02-27 12:34:29 +0100

    r Wrap script i IIFE
    - for å slippe top-level-await

--- src/batch/dump-db-to-kafka.ts
+++ src/batch/dump-db-to-kafka.ts
@@ -1,32 +1,34 @@
 import { PrismaClient } from '@prisma/client';
 import createProducer from '../kafka/automatisk-reaktivert-producer';
 
-const prismaClient = new PrismaClient();
-const kafkaProducer = await createProducer();
+(async function () {
+    const prismaClient = new PrismaClient();
+    const kafkaProducer = await createProducer();
 
-console.log('Starter med å hente automatiskReaktivering...');
-const automatiskReaktivering = await prismaClient.automatiskReaktivering.findMany({
-    where: {
-        created_at: {
-            lt: new Date(),
+    console.log('Starter med å hente automatiskReaktivering...');
+    const automatiskReaktivering = await prismaClient.automatiskReaktivering.findMany({
+        where: {
+            created_at: {
+                lt: new Date(),
+            },
         },
-    },
-});
-console.log(`Fant ${automatiskReaktivering.length} rader`);
-console.log('Dumper automatiskReaktivering på kafka...');
-await kafkaProducer.send(automatiskReaktivering);
-console.log('Ferdig med automatiskReaktivering\n\n\n');
+    });
+    console.log(`Fant ${automatiskReaktivering.length} rader`);
+    console.log('Dumper automatiskReaktivering på kafka...');
+    await kafkaProducer.send(automatiskReaktivering);
+    console.log('Ferdig med automatiskReaktivering\n\n\n');
 
-console.log('Starter med å hente automatiskReaktiveringSvar...');
-const automatiskReaktiveringSvar = await prismaClient.automatiskReaktiveringSvar.findMany({
-    where: {
-        created_at: {
-            lt: new Date(),
+    console.log('Starter med å hente automatiskReaktiveringSvar...');
+    const automatiskReaktiveringSvar = await prismaClient.automatiskReaktiveringSvar.findMany({
+        where: {
+            created_at: {
+                lt: new Date(),
+            },
         },
-    },
-});
+    });
 
-console.log(`Fant ${automatiskReaktiveringSvar.length} rader`);
-console.log('Dumper automatiskReaktiveringSvar på kafka...');
-await kafkaProducer.send(automatiskReaktiveringSvar);
-console.log('Ferdig med automatiskReaktiveringSvar');
+    console.log(`Fant ${automatiskReaktiveringSvar.length} rader`);
+    console.log('Dumper automatiskReaktiveringSvar på kafka...');
+    await kafkaProducer.send(automatiskReaktiveringSvar);
+    console.log('Ferdig med automatiskReaktiveringSvar');
+})();
