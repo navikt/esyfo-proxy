48ddd3297fb9f26407f33ed7aa97299cd675f304
Jostein Holje <jostein.holje@nav.no>
2023-06-15 13:20:13 +0200

    veilederApi: implementer behov-for-veiledning

--- src/api/veileder/index.ts
+++ src/api/veileder/index.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import config from '../../config';
 import { getDefaultHeaders, proxyHttpCall } from '../../http';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { BehovRepository } from '../../db/behovForVeiledningRepository';
 
 function veilederApi(behovForVeiledningRepository: BehovRepository, besvarelseUrl = config.BESVARELSE_URL) {
@@ -12,18 +12,30 @@ function veilederApi(behovForVeiledningRepository: BehovRepository, besvarelseUr
     router.post('/veileder/behov-for-veiledning', async (req, res) => {
         try {
             const { foedselsnummer } = req.body;
+
+            if (!foedselsnummer) {
+                res.status(400).send('missing foedselsnummer');
+                return;
+            }
+
             const { status } = await axios(`${besvarelseUrl}/api/v1/veileder/har-tilgang`, {
                 headers: getDefaultHeaders(req),
                 method: 'POST',
                 data: { foedselsnummer },
             });
 
-            if (status !== 200) {
-                // TODO
+            if (status === 200) {
+                const behov = await behovForVeiledningRepository.hentBehov({ foedselsnummer });
+
+                if (behov) {
+                    res.send({ oppfolging: behov.oppfolging, dato: behov.created_at, dialogId: behov.dialog_id });
+                } else {
+                    res.status(204).end();
+                }
             }
         } catch (err) {
-        } finally {
-            res.status(200).end();
+            const errorResponse = (err as AxiosError).response;
+            res.status(errorResponse?.status || 500).end();
         }
     });
 
