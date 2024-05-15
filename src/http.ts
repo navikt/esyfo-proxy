9ec26efd8e2e7e562e9f5c1e9379b4f756701cdf
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2024-05-15 10:41:37 +0200

    Fix eslint config (#156)

--- src/http.ts
+++ src/http.ts
@@ -1,109 +1,119 @@
-import { Request, Response } from 'express';
-import config from './config';
-import axios, { AxiosError } from 'axios';
-import logger, { axiosLogError, getCustomLogProps } from './logger';
-import { getTokenFromRequest } from './auth/tokenDings';
-import { isNetworkOrIdempotentRequestError } from './isRetryAllowed';
+import { Request, Response } from "express";
+import config from "./config";
+import axios, { AxiosError } from "axios";
+import logger, { axiosLogError, getCustomLogProps } from "./logger";
+import { getTokenFromRequest } from "./auth/tokenDings";
+import { isNetworkOrIdempotentRequestError } from "./isRetryAllowed";
 
 interface ProxyOpts {
-    headers?: Record<string, string | null>;
-    overrideMethod?: string;
-    skipRetry?: boolean;
-    maxRetries?: number;
-    retryDelay?: number;
+  headers?: Record<string, string | null>;
+  overrideMethod?: string;
+  skipRetry?: boolean;
+  maxRetries?: number;
+  retryDelay?: number;
 }
 
 export function getDefaultHeaders(req: Request) {
-    const token = getTokenFromRequest(req);
-    return {
-        'Content-Type': req.header('Content-Type') || 'application/json',
-        ...(req.header('Nav-Call-Id') ? { 'Nav-Call-Id': req.header('Nav-Call-Id') } : {}),
-        ...(req.header('NAV_CSRF_PROTECTION') ? { NAV_CSRF_PROTECTION: req.header('NAV_CSRF_PROTECTION') } : {}),
-        [config.CONSUMER_ID_HEADER_NAME]: config.CONSUMER_ID_HEADER_VALUE,
-        Authorization: `Bearer ${token}`,
-    };
+  const token = getTokenFromRequest(req);
+  return {
+    "Content-Type": req.header("Content-Type") || "application/json",
+    ...(req.header("Nav-Call-Id")
+      ? { "Nav-Call-Id": req.header("Nav-Call-Id") }
+      : {}),
+    ...(req.header("NAV_CSRF_PROTECTION")
+      ? { NAV_CSRF_PROTECTION: req.header("NAV_CSRF_PROTECTION") }
+      : {}),
+    [config.CONSUMER_ID_HEADER_NAME]: config.CONSUMER_ID_HEADER_VALUE,
+    Authorization: `Bearer ${token}`,
+  };
 }
 
 const defaultOpts: ProxyOpts = {
-    skipRetry: false,
-    maxRetries: 3,
-    retryDelay: 20,
+  skipRetry: false,
+  maxRetries: 3,
+  retryDelay: 20,
 };
 
 export function proxyHttpCall(url: string, opts: ProxyOpts = defaultOpts) {
-    return async (req: Request, res: Response) => {
-        const method = opts?.overrideMethod || req.method;
-        const defaultHeaders = getDefaultHeaders(req);
+  return async (req: Request, res: Response) => {
+    const method = opts?.overrideMethod || req.method;
+    const defaultHeaders = getDefaultHeaders(req);
 
-        const retry = async (counter: number): Promise<any> => {
-            const { skipRetry, retryDelay, maxRetries } = opts;
+    const retry = async (counter: number): Promise<unknown> => {
+      const { skipRetry, retryDelay, maxRetries } = opts;
 
-            const shouldRetry = (err: AxiosError) => {
-                return !skipRetry && counter < maxRetries! && isNetworkOrIdempotentRequestError(err);
-            };
+      const shouldRetry = (err: AxiosError) => {
+        return (
+          !skipRetry &&
+          counter < maxRetries! &&
+          isNetworkOrIdempotentRequestError(err)
+        );
+      };
 
-            try {
-                const {
-                    data: bodyStream,
-                    status,
-                    headers,
-                } = await axios(url, {
-                    method,
-                    data: req.method === 'POST' ? req.body : undefined,
-                    params: req.params,
-                    headers: {
-                        ...defaultHeaders,
-                        ...opts?.headers,
-                    },
-                    responseType: 'stream',
-                });
+      try {
+        const {
+          data: bodyStream,
+          status,
+          headers,
+        } = await axios(url, {
+          method,
+          data: req.method === "POST" ? req.body : undefined,
+          params: req.params,
+          headers: {
+            ...defaultHeaders,
+            ...opts?.headers,
+          },
+          responseType: "stream",
+        });
 
-                if (counter > 0) {
-                    console.info(`Vellykket retry etter ${counter} forsøk mot: ${url}`);
-                }
+        if (counter > 0) {
+          console.info(`Vellykket retry etter ${counter} forsøk mot: ${url}`);
+        }
 
-                if (status === 204) {
-                    return res.status(status).end();
-                }
+        if (status === 204) {
+          return res.status(status).end();
+        }
 
-                res.status(status);
-                res.set(headers);
-                return bodyStream.pipe(res);
-            } catch (err) {
-                const axiosError = err as AxiosError;
-                const status = axiosError.response?.status || 500;
+        res.status(status);
+        res.set(headers);
+        return bodyStream.pipe(res);
+      } catch (err) {
+        const axiosError = err as AxiosError;
+        const status = axiosError.response?.status || 500;
 
-                if (shouldRetry(axiosError)) {
-                    logger.warn(`Retry kall ${counter + 1}} mot ${url}: response ${status}`);
-                    return setTimeout(() => retry(counter + 1), retryDelay);
-                }
+        if (shouldRetry(axiosError)) {
+          logger.warn(
+            `Retry kall ${counter + 1}} mot ${url}: response ${status}`,
+          );
+          return setTimeout(() => retry(counter + 1), retryDelay);
+        }
 
-                axiosLogError(axiosError, getCustomLogProps(req));
-                return res.status(status).send((err as Error).message);
-            }
-        };
-
-        return retry(0);
+        axiosLogError(axiosError, getCustomLogProps(req));
+        return res.status(status).send((err as Error).message);
+      }
     };
+
+    return retry(0);
+  };
 }
 
 export function proxyTokenXCall(
-    url: string,
-    getTokenXHeaders: (req: Request) => Promise<Record<string, string | null>>,
-    opts = defaultOpts
+  url: string,
+  getTokenXHeaders: (req: Request) => Promise<Record<string, string | null>>,
+  opts = defaultOpts,
 ) {
-    return async (req: Request, res: Response) => {
-        try {
-            await proxyHttpCall(url, {
-                ...opts,
-                headers: await getTokenXHeaders(req),
-            })(req, res);
-        } catch (err) {
-            logger.error(`proxyTokenXCall error : ${(err as Error).message}`);
-            const axiosError = err as AxiosError;
-            const status = axiosError.response?.status || 500;
-            axiosLogError(axiosError, getCustomLogProps(req));
-            res.status(status).end();
-        }
-    };
+  return async (req: Request, res: Response) => {
+    try {
+      await proxyHttpCall(url, {
+        ...opts,
+        headers: await getTokenXHeaders(req),
+      })(req, res);
+    } catch (err) {
+      logger.error(`proxyTokenXCall error : ${(err as Error).message}`);
+      const axiosError = err as AxiosError;
+      const status = axiosError.response?.status || 500;
+      axiosLogError(axiosError, getCustomLogProps(req));
+      res.status(status).end();
+    }
+  };
 }
--- src/logger.ts
+++ src/logger.ts
@@ -1,62 +1,68 @@
-import pino from 'pino';
-import { pinoHttp } from 'pino-http';
-import ecsFormat from '@elastic/ecs-pino-format';
-import { IncomingMessage, ServerResponse } from 'http';
-import Config from './config';
-import { AxiosError } from 'axios';
+import pino from "pino";
+import { pinoHttp } from "pino-http";
+import ecsFormat from "@elastic/ecs-pino-format";
+import { IncomingMessage, ServerResponse } from "http";
+import Config from "./config";
+import { AxiosError } from "axios";
 
 const logger = pino({
-    ...ecsFormat({ apmIntegration: false }),
-    formatters: {
-        level: (label: string) => ({ level: label }),
-    },
+  ...ecsFormat({ apmIntegration: false }),
+  formatters: {
+    level: (label: string) => ({ level: label }),
+  },
 });
 
-export function customRequestLogMessage(req: IncomingMessage, res: ServerResponse) {
-    return `${req.method} ${req.url} completed ${res.statusCode} ${res.statusMessage}`;
+export function customRequestLogMessage(
+  req: IncomingMessage,
+  res: ServerResponse,
+) {
+  return `${req.method} ${req.url} completed ${res.statusCode} ${res.statusMessage}`;
 }
 
 export function getLogLevel(statusCode: number, err?: Error): pino.Level {
-    if (statusCode === 401 || statusCode === 403) return 'warn';
-    if (statusCode >= 400 || err) return 'error';
-    return 'info';
+  if (statusCode === 401 || statusCode === 403) return "warn";
+  if (statusCode >= 400 || err) return "error";
+  return "info";
 }
 
 export function getCustomLogProps(req: IncomingMessage) {
-    return {
-        x_callId: req.headers['nav-call-id'],
-        x_consumerId: req.headers[Config.CONSUMER_ID_HEADER_NAME],
-    };
+  return {
+    x_callId: req.headers["nav-call-id"],
+    x_consumerId: req.headers[Config.CONSUMER_ID_HEADER_NAME],
+  };
 }
 
 export function pinoHttpMiddleware() {
-    return pinoHttp({
-        autoLogging: {
-            ignore: (req) => (req.url ? /internal/.test(req.url) : false),
-        },
-        customLogLevel: (_, res, err) => getLogLevel(res.statusCode, err),
-        logger,
-        customSuccessMessage: customRequestLogMessage,
-        customErrorMessage: customRequestLogMessage,
-        customErrorObject: (req, res, error, val = {}) => {
-            return {
-                ...val,
-                x_callId: req.headers['nav-call-id'],
-                x_consumerId: req.headers[Config.CONSUMER_ID_HEADER_NAME],
-            };
-        },
-        customProps: getCustomLogProps,
-    });
+  return pinoHttp({
+    autoLogging: {
+      ignore: (req) => (req.url ? /internal/.test(req.url) : false),
+    },
+    customLogLevel: (_, res, err) => getLogLevel(res.statusCode, err),
+    logger,
+    customSuccessMessage: customRequestLogMessage,
+    customErrorMessage: customRequestLogMessage,
+    customErrorObject: (req, res, error, val = {}) => {
+      return {
+        ...val,
+        x_callId: req.headers["nav-call-id"],
+        x_consumerId: req.headers[Config.CONSUMER_ID_HEADER_NAME],
+      };
+    },
+    customProps: getCustomLogProps,
+  });
 }
 
 export function axiosLogError(err: AxiosError, props: any = {}) {
-    const status = err.response?.status || 500;
-    const logLevel = getLogLevel(status);
-    const method = err.request?.method || 'Unknown method';
-    const url = err.config?.url || 'unknown URL';
-    const statusText = err.response?.statusText || '';
-    const data = err.response?.data || {};
-    logger[logLevel]({ ...props, ...data }, `${method} ${url}: ${status} ${statusText}`);
+  const status = err.response?.status || 500;
+  const logLevel = getLogLevel(status);
+  const method = err.request?.method || "Unknown method";
+  const url = err.config?.url || "unknown URL";
+  const statusText = err.response?.statusText || "";
+  const data = err.response?.data || {};
+  logger[logLevel](
+    { ...props, ...data },
+    `${method} ${url}: ${status} ${statusText}`,
+  );
 }
 
 export default logger;
