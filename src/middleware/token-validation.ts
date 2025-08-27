8c8988e0d29350dcdf30cc2048035ea142332367
Bjørn Faaberg <bjorn.faaberg@nav.no>
2025-08-27 10:37:45 +0200

    upgrade jose (#619)
    * Upgrade major version of jose for token-validation
    
    * Change reserved word AS to uppercase
    
    ---------
    
    Co-authored-by: Bjørn Faaberg <bjof@nav.no>

--- src/middleware/token-validation.ts
+++ src/middleware/token-validation.ts
@@ -1,76 +1,90 @@
-import { Request, RequestHandler } from 'express';
-import { getTokenFromRequest } from '../auth/tokenDings';
-import logger, { getCustomLogProps } from '../logger';
+import { Request, RequestHandler } from "express";
+import { getTokenFromRequest } from "../auth/tokenDings";
+import logger, { getCustomLogProps } from "../logger";
 import {
-    createRemoteJWKSet,
-    decodeJwt,
-    FlattenedJWSInput,
-    JWSHeaderParameters,
-    JWTPayload,
-    jwtVerify,
-    KeyLike,
-} from 'jose';
-import config from '../config';
-
-let tokenxJWKSet: (protectedHeader?: JWSHeaderParameters, token?: FlattenedJWSInput) => Promise<KeyLike>;
+  createRemoteJWKSet,
+  decodeJwt,
+  FlattenedJWSInput,
+  JWSHeaderParameters,
+  JWTPayload,
+  jwtVerify,
+  CryptoKey,
+} from "jose";
+import config from "../config";
+let tokenxJWKSet: (
+  protectedHeader?: JWSHeaderParameters,
+  token?: FlattenedJWSInput,
+) => Promise<CryptoKey>;
 const getTokenXJwkSet = () => {
-    if (!tokenxJWKSet) {
-        tokenxJWKSet = createRemoteJWKSet<KeyLike>(new URL(config.TOKEN_X_JWKS_URI));
-    }
+  if (!tokenxJWKSet) {
+    tokenxJWKSet = createRemoteJWKSet(new URL(config.TOKEN_X_JWKS_URI));
+  }
 
-    return tokenxJWKSet;
+  return tokenxJWKSet;
 };
 
-let idPortenJWKSet: (protectedHeader?: JWSHeaderParameters, token?: FlattenedJWSInput) => Promise<KeyLike>;
+let idPortenJWKSet: (
+  protectedHeader?: JWSHeaderParameters,
+  token?: FlattenedJWSInput,
+) => Promise<CryptoKey>;
 const getIdPortenJwkSet = () => {
-    if (!idPortenJWKSet) {
-        idPortenJWKSet = createRemoteJWKSet<KeyLike>(new URL(config.IDPORTEN_JWKS_URI!));
-    }
+  if (!idPortenJWKSet) {
+    idPortenJWKSet = createRemoteJWKSet(new URL(config.IDPORTEN_JWKS_URI!));
+  }
 
-    return idPortenJWKSet;
+  return idPortenJWKSet;
 };
 
-export type AuthLevel = 'Level3' | 'Level4' | 'idporten-loa-substantial' | 'idporten-loa-high';
-export type ValidatedRequest = Request & { user: { level: AuthLevel; ident: string; fnr: string } };
+export type AuthLevel =
+  | "Level3"
+  | "Level4"
+  | "idporten-loa-substantial"
+  | "idporten-loa-high";
+export type ValidatedRequest = Request & {
+  user: { level: AuthLevel; ident: string; fnr: string };
+};
 
 function isTokenX(decodedToken: JWTPayload) {
-    return decodedToken?.iss === process.env.TOKEN_X_ISSUER;
+  return decodedToken?.iss === process.env.TOKEN_X_ISSUER;
 }
 
 async function verifyToken(token: string, decodedToken: JWTPayload) {
-    if (isTokenX(decodedToken)) {
-        return await jwtVerify(token, getTokenXJwkSet(), {
-            algorithms: ['RS256'],
-        });
-    } else {
-        return await jwtVerify(token, getIdPortenJwkSet(), {
-            algorithms: ['RS256'],
-        });
-    }
+  if (isTokenX(decodedToken)) {
+    return await jwtVerify(token, getTokenXJwkSet(), {
+      algorithms: ["RS256"],
+    });
+  } else {
+    return await jwtVerify(token, getIdPortenJwkSet(), {
+      algorithms: ["RS256"],
+    });
+  }
 }
 
 const tokenValidation: RequestHandler = async (req, res, next) => {
-    try {
-        const token = getTokenFromRequest(req);
+  try {
+    const token = getTokenFromRequest(req);
 
-        if (!token) {
-            logger.warn(getCustomLogProps(req), 'Bearer token mangler');
-            res.sendStatus(401);
-            return;
-        }
-
-        const decodedToken = decodeJwt(token);
-        const result = await verifyToken(token, decodedToken);
-        (req as ValidatedRequest).user = {
-            ident: result.payload.sub!,
-            fnr: result.payload.pid as string,
-            level: decodedToken.acr as AuthLevel,
-        };
-        next();
-    } catch (err: any) {
-        logger.warn(getCustomLogProps(req), `Feil ved tokenx validering: ${err.message}`);
-        res.sendStatus(401);
+    if (!token) {
+      logger.warn(getCustomLogProps(req), "Bearer token mangler");
+      res.sendStatus(401);
+      return;
     }
+
+    const decodedToken = decodeJwt(token);
+    const result = await verifyToken(token, decodedToken);
+    (req as ValidatedRequest).user = {
+      ident: result.payload.sub!,
+      fnr: result.payload.pid as string,
+      level: decodedToken.acr as AuthLevel,
+    };
+    next();
+  } catch (err: any) {
+    logger.warn(
+      getCustomLogProps(req),
+      `Feil ved tokenx validering: ${err.message}`,
+    );
+    res.sendStatus(401);
+  }
 };
 
 export default tokenValidation;
