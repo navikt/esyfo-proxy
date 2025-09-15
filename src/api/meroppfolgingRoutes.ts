30a990d52cbc6a1c6206a67dfbfaaf2d143e24b2
Audun Sørheim <audun.sorheim@nav.no>
2025-09-15 12:38:02 +0200

    Add meroppfolging status endpoint

--- src/api/meroppfolgingRoutes.ts
+++ src/api/meroppfolgingRoutes.ts
@@ -1,29 +1,37 @@
-import { Auth, getTokenFromRequest } from "../auth/tokenDings";
+import {Auth, getTokenFromRequest} from "../auth/tokenDings";
 import config from "../config";
-import { Request, Router } from "express";
-import { proxyTokenXCall } from "../http";
+import {Request, Router} from "express";
+import {proxyTokenXCall} from "../http";
 
 function meroppfolgingRoutes(tokenDings: Auth) {
-  const router = Router();
+    const router = Router();
 
-  const getTokenXHeaders = async (req: Request) => {
-    const incomingToken = getTokenFromRequest(req);
-    const access_token = await tokenDings.exchangeIDPortenToken(
-      incomingToken,
-      config.MEROPPFOLGING_BACKEND_CLIENT_ID,
+    const getTokenXHeaders = async (req: Request) => {
+        const incomingToken = getTokenFromRequest(req);
+        const access_token = await tokenDings.exchangeIDPortenToken(
+            incomingToken,
+            config.MEROPPFOLGING_BACKEND_CLIENT_ID,
+        );
+        return {Authorization: `Bearer ${access_token}`};
+    };
+
+    router.get(
+        "/api/meroppfolging/v2/senoppfolging/status",
+        proxyTokenXCall(
+            `${config.MEROPPFOLGING_BACKEND_HOST}/api/v2/senoppfolging/status`,
+            getTokenXHeaders,
+        ),
     );
-    return { Authorization: `Bearer ${access_token}` };
-  };
 
-  router.get(
-    "/api/meroppfolging/v2/senoppfolging/status",
-    proxyTokenXCall(
-      `${config.MEROPPFOLGING_BACKEND_HOST}/api/v2/senoppfolging/status`,
-      getTokenXHeaders,
-    ),
-  );
+    router.get(
+        "/api/mikrofrontend/v1/status",
+        proxyTokenXCall(
+            `${config.MEROPPFOLGING_BACKEND_HOST}/api/mikrofrontend/v1/status`,
+            getTokenXHeaders,
+        ),
+    );
 
-  return router;
+    return router;
 }
 
 export default meroppfolgingRoutes;
